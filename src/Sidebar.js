6aaeb9e336d43b3f519d18232e51bb236cde8c41
samsonajulor <samson.ajulor@decagon.dev>
2022-02-12 00:57:23 +0100

    added the sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Avatar, IconButton} from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+     <div className="sidebar__header">
+      <IconButton>
+      <Avatar/>
+      </IconButton>
+      <div className="sidebar__headerRight">
+       <IconButton>
+       <DonutLargeIcon/>
+       </IconButton>
+       <IconButton>
+       <ChatIcon/>
+       </IconButton>
+       <IconButton>
+       <MoreVertIcon/>
+       </IconButton>
+      </div>
+     </div>
+
+     <div className="sidebar__search"></div>
+     <div className="sidebar__chats"></div>
+    </div>
+  )
+}
+
+export default Sidebar
